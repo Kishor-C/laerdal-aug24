63a70c772fc8a9b15f5b36c888a9544bd0a9fc20
Kishor <kishor1124@gmail.com>
2024-09-23 16:48:27 +0530

    day 3 examples and materials

--- react-cra-examples/react-vit-app/src/App.jsx
+++ react-cra-examples/react-vit-app/src/App.jsx
@@ -1,15 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { SimpleForm } from './components/MyComponents'
-
+import { DemoComponent, Greetings, HelloComponent, SimpleForm, SimpleLogin, SuccessComponent, UsersList } from './components/MyComponents'
+import {Link, Routes, Route} from 'react-router-dom';
 
 function App() {
   
-  
   return (<div>
-    <h2>React App with Vite</h2>
-    <SimpleForm />
+    <h2 className='alert alert-primary text-center'>React App with Vite</h2>
+    <div className='container-fluid'>
+      <Link to='/userList' className='btn btn-primary btn-lg m-1'>User List</Link> 
+      <Link to='/greetings' className='btn btn-primary btn-lg m-1'>Greetings</Link> 
+      <Link to='/simpleForm' className='btn btn-primary btn-lg m-1'>Simple Form</Link> 
+      <Link to='/hello' className='btn btn-primary btn-lg m-1'>Hello Component</Link>
+      <Link to='/demo' className='btn btn-primary btn-lg m-1'>Demo Component</Link> 
+      <Link to='/login' className='btn btn-primary btn-lg m-1'>Login Component</Link> 
+    </div>
+    <div className='container-fluid'>
+      <Routes>
+        <Route path='' element={<SimpleLogin />}></Route>
+        <Route path='login' element={<SimpleLogin />}></Route>
+        <Route path='userList' element={<UsersList />}></Route>
+        <Route path='simpleForm' element={<SimpleForm />}></Route>
+        <Route path='hello' element={<HelloComponent counter="5"/>}></Route>
+        <Route path='demo' element={<DemoComponent counter="20"/>}></Route>
+        <Route path='greetings' 
+          element={<Greetings name="Atharv" age={20} dob={new Date('2003-10-25')} gender="M"/>}></Route>
+        <Route path='success/:user' 
+          element={<SuccessComponent />} />
+      </Routes>
+    </div>
   </div>)
   
 }
--- react-cra-examples/react-vit-app/src/components/MyComponents.jsx
+++ react-cra-examples/react-vit-app/src/components/MyComponents.jsx
@@ -1,4 +1,165 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+
+export function SuccessComponent() {
+    let {user} = useParams(); // success/:user will store value in user parameter
+
+    return (<div>
+        <h2>Hello {user} you logged in</h2>
+    </div>)
+}
+
+export function SimpleLogin() {
+    let [username, setUsername] = useState('');
+    let [password, setPassword] = useState('');
+    // create an instance useNavigate
+    let nav = useNavigate();
+    let handleSubmit = (e)=> {
+        e.preventDefault();
+        nav('/success/'+username); // similar to <Link to="/success/dynamicUrl"
+    }
+    return (<div>
+        <h2>Login Form</h2>
+        <form onSubmit={handleSubmit}>
+            <input type='text' className='form-control form-control-lg w-25' 
+                onChange={e=>setUsername(e.target.value)}   placeholder="Enter Username" /> <br />
+            <input type='password' className='form-control form-control-lg w-25' 
+                onChange={e=>setPassword(e.target.value)} placeholder="Enter Password" />
+            <br /> 
+            <input type='submit' className='btn btn-primary btn-lg w-25'  value='Login' />
+        </form>
+    </div>)
+}
+
+export function UsersList() {
+    const BASE_URL = "https://jsonplaceholder.typicode.com/users";
+    let [users, setUsers] = useState([]); // initially empty array
+    let [error, setError] = useState('');
+    let [id, setId] = useState('');
+    let [entity, setEntity] = useState({})
+    let fetchUserById = () => {
+        axios.get(`${BASE_URL}/${id}`)
+        .then(value=>{setEntity(value.data); setUsers([value.data])})
+        .catch(err => {setError('User with an id '+id+' not found'); setEntity({}); setUsers([])});
+    }
+    let loadUsers = () => {
+        axios.get(BASE_URL).then(value=>setUsers(value.data)).catch(err=>setError('Something went wrong'))
+    }
+    return (<div>
+        <h2>Loading the users from json placeholder</h2>
+        <div>
+            <h3>Search User by id</h3>
+            <input className='w-25 form-control' type = 'number' onChange={e=>setId(e.target.value)} />
+            <button className='btn btn-primary btn-lg' onClick={fetchUserById}>Search</button> <br /><br />
+            {(entity.id == undefined) ? 
+            <div className='text-danger'><h3>{error}</h3></div> : 
+            <div className='text-success'><h3>Hello {entity.username}, you are found</h3></div>}
+        </div>
+        <button onClick={loadUsers} className='btn btn-primary btn-lg'>Fetch Users</button>
+        <table className="table">
+            <thead>
+                <tr><th>Id</th><th>Username</th><th>Email</th><th>Street</th><th>Zip</th></tr>
+            </thead>
+            <tbody>
+                {
+                    users.map((value, index)=>
+                    <tr key={value.id}>
+                        <td>{value.id}</td>
+                        <td>{value.username}</td>
+                        <td>{value.email}</td>
+                        <td>{value.address.street}</td>
+                        <td>{value.address.zipcode}</td>
+                    </tr>)
+                }
+            </tbody>
+        </table>
+    </div>)
+}
+
+// component that uses prop types
+export function Greetings(props) {
+    // assuming we get name, age, dob, gender via props
+    let {name="Guest", age, dob, gender} = props;
+    
+    return (<div>
+       <h2>Greetings component</h2>
+       <p>Hello {name}, age: {age}, 
+        dob: {dob.toLocaleDateString()}, 
+        gender: {gender}
+        </p>
+    </div>)
+}
+// wherever you create the component there you need
+// to define the prop types, propTypes 'p' is lower case
+// while using Greetings.propTypes, however 'p' is upper case
+// while accessing the types, because that's what imported
+Greetings.propTypes = {
+    name : PropTypes.string ,
+    age : PropTypes.number,
+    dob : PropTypes.instanceOf(Date),
+    gender : PropTypes.oneOf(['M', 'F'])
+}
+//Greetings.defaultProps = { name : "Guest" }
+
+
+export function DemoComponent(props) {
+    let {counter} = props;
+    let [name, setName] = useState('Guest');
+    // useEffect must be written only in functional compnt
+    useEffect(()=>{
+        console.log('useEffect with [counter, name]');
+    }, [counter, name]);
+    useEffect(()=>{
+        console.log('useEffect with []')
+    }, []);
+    useEffect(()=>{
+        console.log('useEffect with [counter]')
+    }, [counter]);
+    useEffect(()=>{
+        console.log('useEffect with [name]')
+    }, [name]);
+    useEffect(()=>{ 
+        return ()=>{console.log('component unmount')}
+    },[])
+    return (<div>
+        <h2>Demo Component, counter: {counter}</h2>
+        <button onClick={()=>setName('Kishor'+Math.random())}>
+            MyButton in child
+        </button>
+    </div>)
+
+}
+
+export class HelloComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('constructor() is called');
+    }
+    render() {
+        
+        console.log('render() is called');
+        return (<div>
+            <h2>Counter: {this.props.counter}</h2>
+        </div>)
+    }
+   
+    componentDidUpdate() {
+        // called when state or prop changes
+        console.log('componentDidUpdate() is called');
+    }
+    componentWillUnmount() {
+        // called when component is removed from the DOM
+        console.log('componentWillUnmount() is called');
+    }
+    componentDidMount() {
+        // called once component is added to the DOM
+        console.log('componentDidMount called');
+    }
+
+    
+}
 
 
 export class MyForm extends React.Component {
